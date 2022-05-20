3b8d9370e9c257aaf8af25103ea6eec0a2e4ee51
Manaheeljamil <manaheeljamil20@gmail.com>
2022-05-20 03:46:56 -0700

    First commit

---
+++ note.js
@@ -0,0 +1,143 @@
+// CHAPTER #21 TO 25:
+// Question # 1:
+// var firstName =prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var merge = firstName + " " + lastName;
+// alert ("Welcome!! " + " " + merge);
+
+// Question # 2:
+// var mobile = prompt("What is your favourite mobile model?")
+// var length = mobile.length;
+// document.write("My favourite phone is:" + " " + mobile +"<br/>" );
+// document.write( "length Of String:" + " " + length);
+
+// Question # 3:
+// var name = "Pakistani";
+// var index = name.indexOf("n");
+// document.write("String :"+ " " + name + "<br/>" + "Index of 'n':" + " " + index);
+
+// Question # 4:
+// var name = "Hello World";
+// var index = name.lastIndexOf("l");
+// document.write("String :"+ " " + name + "<br/>" + "Last Index of 'l':" + " " + index);
+
+// Question # 5:
+// var name = "Pakistani";
+// var index = name.charAt(3);
+// document.write("String :"+ " " + name + "<br/>" + "Character at index '3':" + " " + index);
+
+// Question # 6:
+// var firstName = "virginia";
+// var firstName =prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var merge = (firstName + " " + lastName);
+// alert ("Welcome!! " + " " + merge);
+
+// Question # 7:
+// var city = "Hyderabad";
+// var newCity = city.replace("Hyder", "Islam");
+// document.write("City:" + " " + city +"<br/>" + "After Replacement:" + " " + newCity)
+
+// Question # 8:
+// var message = ("Ali and Sami are best friends. They play cricket and football together");
+// var newMessage = message.replace(/and/g , "&");
+// document.write(newMessage) 
+
+// Question # 9:
+// var string = "472";
+// document.write("Value:" + " " + string +"<br/>"+"Type : String" + "<br/>");
+// var string = Number("472");
+// document.write("Value:" + " " + string +"<br/>"+"Type : Number");
+
+// Question # 10:
+// var userInput = "peanuts";
+// var upperCase =  userInput.toUpperCase();
+// document.write("User Input:" + " " + userInput + " <br/>" + "UpperCase :" + " " + upperCase)
+
+// // Question # 11:
+// var userInput = "javascript";
+// var upperCase =  userInput.toTitleCase();
+// document.write("User Input:" + " " + userInput + " <br/>" + "TitleCase :" + " " + upperCase);
+
+// Question # 12:
+// var num =  35.36;
+// var newNum =  num.toString().replace(".", "");
+// document.write("Number" + " " + num + "<br/>" + "Results :" + " " + newNum)
+
+// Question # 13:
+// var password = prompt("Enter a Password?");
+// var array = ["!", ",", ".", "@"];
+// for (var i = 0; i<=array.length; i++) {
+//     if (password[i] !=33 || password[i] !=44 || password[i] != 46 || password[i] != 64) {
+//         alert ("Correct answer");
+//         break;
+//     }
+//     if (password[i] ==33 || password[i] ==44 || password[i] == 46 || password[i] == 64) {
+//         alert ("Please enter a valid password!");
+        
+//     }
+// }
+
+// Question # 14:
+// var input = prompt("what item you selected?");
+// var lowerCase = input.toLowerCase();
+// A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// for (var i=0; i<=A.length ; i++) {
+//     if (lowerCase=== A[i]) {
+//         alert(input + " " + "is available at index" + " " + i + " in our bakery");
+//         break;
+//     }
+// }
+// var upperCase = input.toUpperCase();
+// for (var i=0; i<=A.length ; i++) {
+//     if (upperCase=== A[i]) {
+//         alert(input + " " + "is available at index" + " " + i + " in our bakery");
+//         break;
+//     }
+// }
+// var titleCase = input.toTitleCase();
+// for (var i=0; i<=A.length ; i++) {
+//     if (titleCase=== A[i]) {
+//         alert(input + " " + "is available at index" + " " + i + " in our bakery");
+//         break;
+//     }
+// }
+// if (input !== A[i]) {
+//    alert("We are sorry." + input + "is not available in our bakery");
+//   }
+
+// Question # 15:
+// var password = prompt("Enter a password!");
+// document.write("Enter password :" + " " + password +"<br/>")
+// for (var i =0; i<=100; i++) {
+// if (password.indexOf === [i]) {
+//    document.write("Password cannot begin with a number " + "<br/>" +"Please enter a valid password");
+//     break;
+// }
+// if (password.length > 6 ) {
+//     document.write("Password length must be at least 6 character " + "<br/>" + "Please enter a valid password");
+//     break;
+// }
+// if (password.length === 6 || password.indexOf !== [i]) {
+//     document.write("Correct answer");
+//     break;
+// }
+// }
+
+// Question # 16:
+// var university = "University of Karachi";
+// for (var i =0; i< university.length; i++) {
+//     document.write(university[i] + "<br/>");
+// }
+
+// Question # 17:
+// var userInput = "Pakistan";
+// var output = userInput.charAt(userInput.length-1);
+// document.write("UserInput:" + " " + userInput+"<br/>"+ "Last character of input:"+" "+ output)
+
+// Question # 18:
+// var string = "The quick brown fox jumps over the lazy dog";
+// if ()
+// var newString = string.charAt();
+// document.write(newString)
+// nh hpo raha
